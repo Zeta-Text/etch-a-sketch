6cd34ad450643e652abe09a9bc34b3dbc24d9213
Zeta-Text <holdenwllms@outlook.com>
2022-02-08 01:56:15 -0700

    Added div grid, and some styling.

---
+++ javascript.js
@@ -0,0 +1,98 @@
+const container = document.querySelector('#container');
+//---------------------
+const boxOne = document.createElement('div');
+boxOne.classList.add('boxOne');
+
+
+container.appendChild(boxOne);
+//---------------------
+const boxTwo = document.createElement('div');
+boxTwo.classList.add('boxTwo');
+
+
+container.appendChild(boxTwo);
+//----------------------
+const boxThree = document.createElement('div');
+boxThree.classList.add('boxThree');
+
+
+container.appendChild(boxThree);
+//----------------------
+const boxFour = document.createElement('div');
+boxFour.classList.add('boxFour');
+
+
+container.appendChild(boxFour);
+//----------------------
+const boxFive = document.createElement('div');
+boxFive.classList.add('boxFive');
+
+
+container.appendChild(boxFive);
+//-----------------------
+const boxSix = document.createElement('div');
+boxSix.classList.add('boxSix');
+
+
+container.appendChild(boxSix);
+//-----------------------
+const boxSeven = document.createElement('div');
+boxSeven.classList.add('boxSeven');
+
+
+container.appendChild(boxSeven);
+//-----------------------
+const boxEight = document.createElement('div');
+boxEight.classList.add('boxEight');
+
+
+container.appendChild(boxEight);
+//-----------------------
+const boxNine = document.createElement('div');
+boxNine.classList.add('boxNine');
+
+
+container.appendChild(boxNine);
+//-----------------------
+const boxTen = document.createElement('div');
+boxTen.classList.add('boxTen');
+
+
+container.appendChild(boxTen);
+//-----------------------
+const box11 = document.createElement('div');
+box11.classList.add('box11');
+
+
+container.appendChild(box11);
+//-----------------------
+const box12 = document.createElement('div');
+box12.classList.add('box12');
+
+
+container.appendChild(box12);
+//-----------------------
+const box13 = document.createElement('div');
+box13.classList.add('box13');
+
+
+container.appendChild(box13);
+//-----------------------
+const box14 = document.createElement('div');
+box14.classList.add('box14');
+
+
+container.appendChild(box14);
+//-----------------------
+const box15 = document.createElement('div');
+box15.classList.add('box15');
+
+
+container.appendChild(box15);
+//-----------------------
+const box16 = document.createElement('div');
+box16.classList.add('box16');
+
+
+container.appendChild(box16);
+//-----------------------
