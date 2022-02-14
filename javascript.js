99c7a1bdb0112f6e71e4d28f90425267536d99b6
Zeta-Text <holdenwllms@outlook.com>
2022-02-14 00:26:15 -0700

    Added drawing function.

--- javascript.js
+++ javascript.js
@@ -1,98 +1,59 @@
 const container = document.querySelector('#container');
-//---------------------
-const boxOne = document.createElement('div');
-boxOne.classList.add('boxOne');
+//------------------------
 
 
-container.appendChild(boxOne);
-//---------------------
-const boxTwo = document.createElement('div');
-boxTwo.classList.add('boxTwo');
+function loadPage() {
+    let w = 16;
+    const container = document.getElementById("container");
+    const newW = 600 / w;
+    
+    for (let i = 0; i < w * w; i++) {
+        let newBox = document.createElement('div');
+        newBox.classList.add('newBox')
+        newBox.style.setProperty('--newBox-w', newW + "px");
+        container.appendChild(newBox);
+
+        
+        
+
+    }
+}
+
+loadPage();
 
 
-container.appendChild(boxTwo);
-//----------------------
-const boxThree = document.createElement('div');
-boxThree.classList.add('boxThree');
+
+//------------------------
+let newDimension = "";
+
+function reset() {
+    const container = document.getElementById("container");
+    container.innerHTML = '';
+    return;
+        
+    
+}
+
+function createGrid() {
+    let newDimension = prompt("Enter the dimension of the canvas. (eg: 41)");
+    const container = document.getElementById("container");
+    const newWidth = 600 / newDimension;
+    
+    for (let i = 0; i < newDimension * newDimension; i++) {
+        let newBox = document.createElement('div');
+        newBox.classList.add('newBox')
+        newBox.style.setProperty('--newBox-w', newWidth + "px");
+        container.appendChild(newBox);
+
+        
+        
+
+    }
+}
 
 
-container.appendChild(boxThree);
-//----------------------
-const boxFour = document.createElement('div');
-boxFour.classList.add('boxFour');
+btn.addEventListener('click',() => {    
+       reset()
+       createGrid()
 
-
-container.appendChild(boxFour);
-//----------------------
-const boxFive = document.createElement('div');
-boxFive.classList.add('boxFive');
-
-
-container.appendChild(boxFive);
-//-----------------------
-const boxSix = document.createElement('div');
-boxSix.classList.add('boxSix');
-
-
-container.appendChild(boxSix);
-//-----------------------
-const boxSeven = document.createElement('div');
-boxSeven.classList.add('boxSeven');
-
-
-container.appendChild(boxSeven);
-//-----------------------
-const boxEight = document.createElement('div');
-boxEight.classList.add('boxEight');
-
-
-container.appendChild(boxEight);
-//-----------------------
-const boxNine = document.createElement('div');
-boxNine.classList.add('boxNine');
-
-
-container.appendChild(boxNine);
-//-----------------------
-const boxTen = document.createElement('div');
-boxTen.classList.add('boxTen');
-
-
-container.appendChild(boxTen);
-//-----------------------
-const box11 = document.createElement('div');
-box11.classList.add('box11');
-
-
-container.appendChild(box11);
-//-----------------------
-const box12 = document.createElement('div');
-box12.classList.add('box12');
-
-
-container.appendChild(box12);
-//-----------------------
-const box13 = document.createElement('div');
-box13.classList.add('box13');
-
-
-container.appendChild(box13);
-//-----------------------
-const box14 = document.createElement('div');
-box14.classList.add('box14');
-
-
-container.appendChild(box14);
-//-----------------------
-const box15 = document.createElement('div');
-box15.classList.add('box15');
-
-
-container.appendChild(box15);
-//-----------------------
-const box16 = document.createElement('div');
-box16.classList.add('box16');
-
-
-container.appendChild(box16);
-//-----------------------
+});
